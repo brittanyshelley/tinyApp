e4507a1d8c723d80e1b5f70e97feee8a574ba75d
brittanyshelley <brittany.j.shelley@gmail.com>
2023-11-02 23:10:28 +0000

    added page with urlDatabase

--- express_server.js
+++ express_server.js
@@ -10,6 +10,11 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+//added page to display urls
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
