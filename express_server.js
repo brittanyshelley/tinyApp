94748f2af77ecb65d051a5f1b5a738a4b1d610e1
brittanyshelley <brittany.j.shelley@gmail.com>
2023-11-04 11:46:39 -0600

    fixed longURL connection

--- express_server.js
+++ express_server.js
@@ -8,10 +8,27 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id]; // Retrieve the long URL from urlDatabase
+
+  const templateVars = { id: id, longURL: longURL };
+  res.render("urls_show", templateVars);
+
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 app.get("/", (req, res) => {
   res.send("Hello!");
