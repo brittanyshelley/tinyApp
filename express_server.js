861257723fc2ee04d8be0bf60db1a51c58db30c2
brittanyshelley <brittany.j.shelley@gmail.com>
2023-12-06 20:56:43 -0700

    made error fixes in urls route

--- express_server.js
+++ express_server.js
@@ -33,6 +33,11 @@ const users = {
     email: "user2@example.com",
     password: "dishwasher-funk",
   },
+  "aJ48lW": {
+    id: "aJ48lW",
+    email: "test@test.com",
+    password: "test",
+  }
 };
 
 
@@ -46,70 +51,86 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(cookieSession({
-   name: 'session',
-   keys: ['cookieKey'],
+  name: 'session',
+  keys: ['cookieKey'],
 }));
 
 // Home
 app.get("/", (req, res) => {
-  const loggedInUser = req.session.user_id;//Get the user_id from the session
-    if (loggedInUser) {
-      return res.redirect('/urls');
-    }
-  if (!loggedInUser) {
-    return res.redirect('/login');
-  }
-});
-
-// ***************************
-app.get("/urls", (req, res) => {
-  const loggedInUser = req.session.user_id;
+  const userID = req.session.user_id;
+  const loggedInUser = users[userID];//Get the user_id from the session
   if (loggedInUser) {
-    const userURLs = urlsForUser(loggedInUser, urlDatabase);
-    const templateVars = {
-      urls: userURLs,
-      user: users[loggedInUser]
-    };
-    console.log('templateVars:', templateVars);
-    return res.render("urls_index", templateVars);
-  } else {
-    return res.send('<h2>you must be signed in to use this page</h2>');
+    return res.redirect('/urls');
   }
-});
-
-
-app.get("/urls/new", (req, res) => {
-  const loggedInUser = req.session.user_id;
-  // console.log('loggedInUsure:', loggedInUser);
   if (!loggedInUser) {
     return res.redirect('/login');
   }
+});
+
+// GET /urls route
+app.get("/urls", (req, res) => {
+  const userId = req.session.user_id;
+  const loggedInUser = users[userId];
+  if (!loggedInUser) {
+    return res.send('<h2>You must be signed in to use this page</h2>');
+  }
+
+  const userUrls = urlsForUser(userId, urlDatabase);
+  const templateVars = {
+    urls: userUrls,
+    user: loggedInUser
+  };
+  return res.render("urls_index", templateVars);
+});
+
+// GET /urls/new route
+app.get("/urls/new", (req, res) => {
+  const userID = req.session.user_id;
+  const loggedInUser = users[userID];
+
+  if (!loggedInUser) {
+    return res.redirect('/login');
+  }
+
   //Passing user object to templateVars
   const templateVars = {
     urls: urlDatabase,
     user: users[loggedInUser]
   };
+
   res.render("urls_new", templateVars);
 });
 
-
+// GET /urls/:id route
 app.get("/urls/:id", (req, res) => {
-  const loggedInUser = req.session.user_id;
+  const userID = req.session.user_id;
+  const loggedInUser = users[userID];
   if (!loggedInUser) {
-    return res.send('<h2>you must be signed in to use this page</h2>');
+    return res.send('<h2>You must be signed in to use this page</h2></br></br><a href="/login">Login to continue</a>');
   }
-  if (loggedInUser !== urlDatabase[req.params.id].userID) {
-    return res.send('<h2>you do not have permission to edit this URL</h2>');
+
+
+  // Check if the url belongs to the user
+  const shortUrl = req.params.id;
+  if (loggedInUser) {
+    const userURLs = urlsForUser(userID, urlDatabase);
+    console.log("userURLs:_____________", userURLs)
+    if (userURLs[shortUrl].userID === userID) {
+      const templateVars = {
+        id: shortUrl,
+        longURL: userURLs[shortUrl].longURL,
+        user: loggedInUser
+      };
+
+      return res.render("urls_show", templateVars);
+    }
+    else {
+      res.send('<h2>You do not have permission to edit this URL</h2>');
+    }
   }
-  const templateVars = {
-    urls: urlDatabase,
-    user: users[loggedInUser]
-  };
- console.log("templateVars:", templateVars);
-  res.render("urls_show", templateVars);
 });
 
-// Redirects to the longURL page
+// GET /u/:id route
 app.get("/u/:id", (req, res) => {
   if (urlDatabase[req.params.id]) {
     const longURL = urlDatabase[req.params.id];
@@ -119,33 +140,52 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
+// Complete the POST /urls route
 app.post("/urls", (req, res) => {
-  const loggedInUser = req.session.user_id;
+  const userID = req.session.user_id;
+  const loggedInUser = users[userID];
   if (!loggedInUser) {
-    return res.status(401).send('you must be signed in to use this page');
+    return res.status(401).send('You must be signed in to use this page');
   }
   const longURL = req.body.longURL;
   const id = generateRandomString();
-  // urlDatabase[id] = newLongURL;
   urlDatabase[id] = {
     longURL,
-    userID: loggedInUser,
+    userID
   };
   console.log("urlDatabase:_____________", urlDatabase);
   res.redirect(`/urls/${id}`);
-
 });
+
 //Update's url from database when button pushed
 app.post('/urls/:id', (req, res) => {
-  // console.log(req.session.user_id, '***');
+  const userID = req.session.user_id;
+  const loggedInUser = users[userID];
+  if (!loggedInUser) {
+    return res.send('<h2>You must be signed in to use this page</h2>');
+  }
+
   const id = req.params.id;
-  const newLongURL = req.body.newLongURL;
-  urlDatabase[id] = newLongURL
+  const longURL = req.body.newLongURL;
+  const shortUrl = urlDatabase[id];
+
+  // is this url owned by the logged in user?
+  if (shortUrl.userID !== userID) {
+    return res.send('<h2>You do not have permission to edit this URL</h2>');
+  }
+
+  shortUrl.longURL = longURL;
   res.redirect('/urls');
 });
 
 //Delete's url from database when button pushed
 app.post("/urls/:id/delete", (req, res) => {
+  const userID = req.session.user_id;
+  const loggedInUser = users[userID];
+  if (!loggedInUser) {
+    return res.send('<h2>You must be signed in to use this page</h2>');
+  }
+
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect("/urls");
@@ -153,14 +193,16 @@ app.post("/urls/:id/delete", (req, res) => {
 
 // GET login
 app.get("/login", (req, res) => {
-  const loggedInUser = req.session.user_id;//Get the user_id from the session
-    if (loggedInUser) {
-      return res.redirect('/urls');
-    }
-    const templateVars = { user: null };
+  const userID = req.session.user_id;
+  const loggedInUser = users[userID];//Get the user_id from the session
+  if (loggedInUser) {
+    return res.redirect('/urls');
+  }
+  const templateVars = { user: null };
   res.render("urls_login", templateVars);
 });
 
+//  Complete the POST /login route
 //POST /login
 app.post("/login", (req, res) => {
   const email = req.body.email;
@@ -169,31 +211,39 @@ app.post("/login", (req, res) => {
   if (!email || !password) {
     return res.status(403).send('please provide an email AND password');
   }
+
   // lookup the user based on their email
   const foundUser = getUserByEmail(email, users);
   // did we NOT find a user
   if (!foundUser) {
     return res.status(403).send('no user with that email found');
   }
+
   // do the passwords NOT match
   const result = bcrypt.compareSync(password, foundUser.password);
   //if (foundUser.password !== password) {
   if (!result) {
     return res.status(403).send('passwords do not match');
   }
+
   req.session.user_id = foundUser.id;
   res.redirect('/urls');
 });
 
+// Complete the GET /register route
 app.get('/register', (req, res) => {
-  const loggedInUser = req.session.user_id;//Get the user_id from the session
-    if (loggedInUser) {
-      return res.redirect('/urls');
-    }
+  const userID = req.session.user_id;
+  const loggedInUser = users[userID];//Get the user_id from the session
+
+  if (loggedInUser) {
+    return res.redirect('/urls');
+  }
+
   const templateVars = { user: null };
   res.render('urls_register', templateVars);
 });
 
+// Complete the POST /register route
 // Handle the registration logic here
 app.post('/register', (req, res) => {
   const { email, password } = req.body;
@@ -218,10 +268,10 @@ app.post('/register', (req, res) => {
   };
   users[id] = user; // Update the users object with the new user
   req.session.user_id = id; // Setting the cookie here
-  console.log('users:', users);
   res.redirect('/urls');
 });
 
+// Complete the POST /logout route
 //Logout, clear username cookie and redirect to /urls
 app.post('/logout', (req, res) => {
   req.session.user_id = null; // clear all cookies
@@ -229,6 +279,6 @@ app.post('/logout', (req, res) => {
 });
 
 app.listen(PORT, () => {
-   console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
--- function.js
+++ function.js
@@ -54,6 +54,7 @@ const urlsForUser = (id, urlDatabase) => {
   return userURLs;
 };
 
+
 const urlsForUserId = (id, urlDatabase) => {
   for (let id in urlDatabase) {
     console.log(`Short URL: ${id}, Long URL: ${urlDatabase[id].longURL}`);
